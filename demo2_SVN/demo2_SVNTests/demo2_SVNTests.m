6db69d9e67205d5f82273829e82b29d1f690092c
Gili <942718773@qq.com>
2015-10-30 11:13:36 +0800

    版本一
    已发布

---
+++ demo2_SVN/demo2_SVNTests/demo2_SVNTests.m
@@ -0,0 +1,39 @@
+//
+//  demo2_SVNTests.m
+//  demo2_SVNTests
+//
+//  Created by ios on 15/10/30.
+//  Copyright © 2015年 Tarena. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface demo2_SVNTests : XCTestCase
+
+@end
+
+@implementation demo2_SVNTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
